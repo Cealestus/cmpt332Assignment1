03a398b240961de3d8d4d15e6f42c6b79815bfdb
Ryan Hodgson <rch598@tux1.usask.ca>
2015-09-26 11:57:02 -0600

    added fgets to osh.c

--- osh.c
+++ osh.c
@@ -11,39 +11,61 @@
 
 
 int
-main(int argc, char *argv[])
+main()
 {
+	
+	char ar[30];
+	fgets(ar , 40, stdin);
+	char *str2 = "exit";
+
+	if (strncmp(ar, str2, 1000)){
+		return 0;
+	}
+
+	printf( "%s\n" , ar);
+	
+
+
+
+
+
+
+
+
+/*		
+	fgets(      );
 
 	char * pch;
-	pch = strttok( str, " \n");
+	pch = strtok( argv[], " \n");
 	
 	while(pch != NULL){
-	       pch = strtok( NULL, " \n ");
+		
+		pch = strtok( NULL, " \n ");
 	}
 	
 
 
 
 	int rc = fork();
-        /* fork failed; exit*/
+        /* fork failed; exit
 	if (rc < 0) {
 		fprintf(stderr, "fork failed\n");
 		exit(1);
-	/* child: redirect standard output to a file*/
+	/* child: redirect standard output to a file
 	} else if (rc == 0) { 
 		close(STDOUT_FILENO);
 		open("./p4.output", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
 			
-		/* now exec "wc"...*/
+		/* now exec "wc"...
 		char *myargs[3];
 		myargs[0] = strdup("wc");
 		myargs[1] = strdup("p4.c");
 		myargs[2] = NULL;
 		execvp(myargs[0], myargs);
-	} else { /* parent*/
+	} else { /* parent
 	int wc = wait(NULL);
 	}
-
+*/
 	return 0;
 }
 
