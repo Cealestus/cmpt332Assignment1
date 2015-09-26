c4ca8b0866b658efcf4f2ca3b88c1c3fb3c9bbe9
Ryan Hodgson <rch598@tux1.usask.ca>
2015-09-26 13:07:58 -0600

    fixed warnings when compiling

--- osh.c
+++ osh.c
@@ -14,13 +14,12 @@ int
 main()
 {
 	char *line = NULL;
-	ssize_t bufsize = 0;
+	size_t bufsize = 0;
+	char ** chArray = malloc(128 * sizeof(char*));
+	char *pch;
+	int pos = 0;
 	getline(&line, &bufsize, stdin);
 	
-	char **chArray  = malloc(128 * sizeof(char*));
-	char *pch;
-
-	int pos=0;
 	pch = strtok( line, " \n");
 
 	while(pch != NULL) {
@@ -38,22 +37,22 @@ main()
 
 /*
 	int rc = fork();
-        /* fork failed; exit
+        ... fork failed; exit
 	if (rc < 0) {
 		fprintf(stderr, "fork failed\n");
 		exit(1);
-	/* child: redirect standard output to a file
+	... child: redirect standard output to a file
 	} else if (rc == 0) { 
 		close(STDOUT_FILENO);
 		open("./p4.output", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
 			
-		/* now exec "wc"...
+		... now exec "wc"...
 		char *myargs[3];
 		myargs[0] = strdup("wc");
 		myargs[1] = strdup("p4.c");
 		myargs[2] = NULL;
 		execvp(myargs[0], myargs);
-	} else { /* parent
+	} else { ... parent
 	int wc = wait(NULL);
 	}
 */
